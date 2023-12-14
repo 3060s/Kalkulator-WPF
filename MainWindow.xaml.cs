139d369e212330950c3b9e50d55dd4f165321c9e
3060s <78622374+3060s@users.noreply.github.com>
2023-12-14 20:40:26 +0100

    Finalna wersja (raczej)

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,102 +1,123 @@
-﻿using System.Text;
-using System.Linq;
+﻿using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace Kalkulator_WPF
+namespace Kalkulator_WPF;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    private const string inputPattern = @"^\d*,?\d*[+\-x÷]?(\d*.?\d*)?$";
+    private string display = "0";
+    private bool ClearScreen = true;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        Update();
+    }
+
+    private void Pierwiastek(object sender, RoutedEventArgs e)
     {
 
-        private string display = "0";
-        private bool ClearScreen = true;
+    }
 
+    private void Potega(object sender, RoutedEventArgs e)
+    {
 
+    }
 
-        public MainWindow()
-        {
-            InitializeComponent();
-            Update();
-        }
+    private void Czyszczenie(object sender, RoutedEventArgs e)
+    {
+        display = "0";
+        ClearScreen = true;
+        Update();
+    }
 
-        private void Pierwiastek(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Potega(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Czyszczenie(object sender, RoutedEventArgs e)
+    private void Cofanie(object sender, RoutedEventArgs e)
+    {
+        if (display == "0") { return; }
+        if (display.Length == 1 || display.Length == 2 && display.Contains('-'))
         {
             display = "0";
             ClearScreen = true;
-            Update();
         }
-
-        private void Cofanie(object sender, RoutedEventArgs e)
+        else
         {
-            if (display == "0") { return; }
-            if (display.Length == 1 || display.Length == 2 && display.Contains("-"))
-            {
-                display = "0";
-                ClearScreen = true;
-            }
-            else
-            {
-                display = display.Remove(display.Length - 1, 1);
-            }
-            Update();
+            display = display.Remove(display.Length - 1, 1);
         }
-
-        private void Input(object sender, RoutedEventArgs e)
-        {
-            string num = (sender as Button).Name.ToString().Remove(0, 1);
-
-            if (display[0] == '0' && num == "0") { return; }
-            if (ClearScreen) { display = ""; }
-
-            display += num;
-            ClearScreen = false;
-            Update();
-        }
-
-        private void Arytmetyka(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Przecinek(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Rownanie(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
-        {
-            base.OnMouseLeftButtonDown(e);
-            this.DragMove();
-        }
-
-        private void Update()
-        {
-            WynikEkran.Text = display;
-        }
+        Update();
     }
+
+    private void Input(object sender, RoutedEventArgs e)
+    {
+        string num = (sender as Button).Content.ToString();
+
+        if (display[0] == '0' && num == "0") { return; }
+        if (ClearScreen) { display = ""; }
+
+        var nextDisplay = display + num;
+
+        if (!InputPattern().Match(nextDisplay).Success)
+        {
+            return;
+        }
+
+        display = nextDisplay;
+        ClearScreen = false;
+        Update();
+    }
+
+    private void Arytmetyka(object sender, RoutedEventArgs e)
+    {
+        string art = (sender as Button).Content.ToString();
+        var nextDisplay = display + art;
+
+        if (!InputPattern().Match(nextDisplay).Success)
+        {
+            return;
+        }
+
+        display = nextDisplay;
+        ClearScreen = false;
+        Update();
+    }
+
+    private void Przecinek(object sender, RoutedEventArgs e)
+    {
+        var nextDisplay = display + ',';
+
+        if (!InputPattern().Match(nextDisplay).Success)
+        {
+            return;
+        }
+
+        display = nextDisplay;
+        ClearScreen = false;
+        Update();
+    }
+
+    public void Rownanie(object sender, RoutedEventArgs e)
+    {
+        ClearScreen = true;
+        display = Obliczenia.CalculateString(display).ToString();
+        Update();
+    }
+
+    protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+    {
+        base.OnMouseLeftButtonDown(e);
+        DragMove();
+    }
+
+    private void Update()
+    {
+        WynikEkran.Text = display;
+    }
+
+    [GeneratedRegex(inputPattern)]
+    private static partial Regex InputPattern();
 }
---
+++ Obliczenia.cs
@@ -0,0 +1,44 @@
+﻿namespace Kalkulator_WPF;
+
+public class Obliczenia
+{
+    public static double CalculateString(string input)
+    {
+        var numbersStr = new string[2];
+        var currentNumber = 0;
+        char op = '\0';
+
+        foreach (var c in input)
+        {
+            if (char.IsDigit(c) || c == ',')
+            {
+                numbersStr[currentNumber] += c;
+            }
+            else
+            {
+                op = c;
+                currentNumber++;
+            }
+        }
+
+        var number1 = double.Parse(numbersStr[0]);
+        var number2 = double.Parse(numbersStr[1]);
+
+        switch (op)
+        {
+            case '+':
+                return number1 + number2;
+
+            case '-':
+                return number1 - number2;
+
+            case 'x':
+                return number1 * number2;
+
+            case '÷':
+                return number1 / number2;
+        }
+
+        return 0;
+    }
+}
